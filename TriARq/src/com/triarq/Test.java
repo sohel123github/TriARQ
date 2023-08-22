ea126d6e25714df183753b557af6bda04c48a15f
Sohel <patvekarsuhel@gmail.com>
2023-08-22 20:47:57 +0530

    First commit

---
+++ TriARq/src/com/triarq/Test.java
@@ -0,0 +1,31 @@
+package com.triarq;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+//	1. (Reverse string + Palindrome or not)	
+		/*String name="wow";
+		int l=name.length();
+		String rev="";
+		for(int i=l-1; i>=0; i--) {
+			rev=rev+name.charAt(i);
+		}
+		System.out.println(rev);
+		if(name.equals(rev)) {
+			System.out.println("Palindrome Sring");
+		}else {
+			System.out.println("Not a Palindrome Sring");
+		}*/
+		
+//  2. (Palindrome number or not)	
+		
+		
+	
+
+
+		
+		
+		
+}
+}
